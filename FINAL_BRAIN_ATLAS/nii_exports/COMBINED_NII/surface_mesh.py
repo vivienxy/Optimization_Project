86d377d90d99ad579d1a9371c8b2ed775afbce75
Vivien Yu <vivienyu@eduroam-campus-10-36-104-43.campus-dynamic.uwaterloo.ca>
2025-11-21 16:27:06 -0500

    changed plot code

--- FINAL_BRAIN_ATLAS/nii_exports/COMBINED_NII/surface_mesh.py
+++ FINAL_BRAIN_ATLAS/nii_exports/COMBINED_NII/surface_mesh.py
@@ -1,5 +1,9 @@
-from vedo import Volume, VolumeToMesh
+from vedo import Volume, Mesh
 
 vol = Volume("brain_mask.nii")
-mesh = VolumeToMesh(vol)
+
+mesh = vol.isosurface(0.5)
+mesh.compute_normals().smooth()
+
 mesh.write("brain_surface.stl")
+print("Saved brain_surface.stl")
